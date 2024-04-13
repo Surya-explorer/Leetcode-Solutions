16dc60388b0ab1914448611edbfad5b187fa7386
Surya Prakash Gupta <113275807+Surya-explorer@users.noreply.github.com>
2024-04-14 00:41:34 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (11.80%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void solve (vector<int>nums , vector< vector<int> > &ans , vector<int>part , int index )  {
+        if (index == nums.size()) {
+            ans.push_back(part) ;
+            return ;
+        }
+        // Excluding nums[index]
+        solve(nums , ans , part , index + 1) ;
+
+        // Including nums[i]
+        int element = nums[index] ;
+        part.push_back(element) ;
+        solve(nums , ans , part , index + 1) ;
+        part.pop_back() ;
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector <vector <int>> ans ;
+        vector <int> part;
+        solve (nums , ans , part  , 0) ;
+        return ans ;
+    }
+};
