da5d9ebe45e011dfcf2525866f5d235569acb96a
Surya Prakash Gupta <113275807+Surya-explorer@users.noreply.github.com>
2024-04-17 18:37:13 +0530

    Time: 8 ms (44.86%) | Memory: 16.3 MB (33.48%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -10,19 +10,18 @@ public:
         while (index < n && target-candidates[index] >= 0){
             // sum += candidates[index] ;
             temp.push_back(candidates[index]) ;
-
             solve(ans , target - candidates[index] , candidates ,index , temp) ;
             index++ ;
             temp.pop_back() ;
+
         }
     }
+
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector <vector <int>> ans  ;
-
         sort(candidates.begin(), candidates.end());
- 
         // remove duplicates
-        candidates.erase(unique(candidates.begin(), candidates.end()), candidates.end());
+        // candidates.erase(unique(candidates.begin(), candidates.end()), candidates.end());
         int index = 0 ;
         vector <int> temp ;
         int n = candidates.size() ;
