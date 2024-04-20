5b4ba51293557e3a80a18ab6ed6d9de5e3aa1f84
Surya Prakash Gupta <113275807+Surya-explorer@users.noreply.github.com>
2024-04-20 19:26:59 +0530

    Time: 11 ms (5.59%) | Memory: 15.8 MB (9.74%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,13 +1,26 @@
 class Solution {
 public : 
     void solve ( vector <int> nums , vector <vector<int>> &ans , vector <int> a , int i  , int n){
-        ans.push_back(a) ;
-        for (int j = i ; j < n ; j++){
-            if (j != i && nums[j] == nums[j-1] ) continue ;
-            a.push_back(nums[j]) ;
-            solve(nums, ans , a , j+1 , n) ;
-            a.pop_back() ;
+        // ans.push_back(a) ;
+        // for (int j = i ; j < n ; j++){
+        //     if (j != i && nums[j] == nums[j-1] ) continue ;
+        //     a.push_back(nums[j]) ;
+        //     solve(nums, ans , a , j+1 , n) ;
+        //     a.pop_back() ;
+        // }
+
+        if (i == n ){
+            ans.push_back(a) ;
+            return ;
         }
+        // including the number
+        a.push_back(nums[i]) ;
+        solve (nums , ans , a , i+1 , n);
+        
+        // Excluding the number
+        a.pop_back() ;
+        while (i+1 < n && nums[i] == nums[i+1]) i++ ;
+        solve (nums , ans , a , i+1 , n);
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector <vector<int>> ans ;
