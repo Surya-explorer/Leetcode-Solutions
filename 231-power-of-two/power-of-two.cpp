734eb2bb6edb8ae3c534cb420b12db6c647e79bf
Surya Prakash Gupta <113275807+Surya-explorer@users.noreply.github.com>
2024-05-29 01:22:18 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (36.59%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -5,10 +5,11 @@ public:
         int cnt = 0 ;
         if (n < 0) return false ;
         while (n != 0 ){
-            // n = n & (n-1) ;
-            if (n%2 == 1) cnt++   ;
-            n = n / 2 ;
-            // cnt ++ ;
+            n = n & (n-1) ;
+            cnt ++ ;
+            
+            // if (n%2 == 1) cnt++   ;
+            // n = n / 2 ;
         }
         if (cnt == 1) return true  ;
         else    return false ;     
