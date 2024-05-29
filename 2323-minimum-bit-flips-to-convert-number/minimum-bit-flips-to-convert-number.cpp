782692e00fb8c160da9b3c9376e5a195f14eb023
Surya Prakash Gupta <113275807+Surya-explorer@users.noreply.github.com>
2024-05-30 02:26:34 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (68.26%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -4,25 +4,25 @@ public:
         
         // Answer is the number of the set bits in the xor of the two numbers .
         
-        int xor1 = (start^goal) ;
-        int cnt = 0 ;
-        for (int i = 0 ; i<=31 ; i++){
-
-            if (xor1 & (1<< i)){
-                cnt ++ ;
-            }
-        }
-        return cnt ;
-
-
-        // int xor1 = (start)^(goal) ;
+        // int xor1 = (start^goal) ;
         // int cnt = 0 ;
+        // for (int i = 0 ; i<=31 ; i++){
 
-        // while (xor1 != 0 ){
-        //     xor1 = xor1 & (xor1-1) ;
-
-        //     cnt ++ ;
+        //     if (xor1 & (1<< i)){
+        //         cnt ++ ;
+        //     }
         // }
         // return cnt ;
+
+
+        int xor1 = (start)^(goal) ;
+        int cnt = 0 ;
+
+        while (xor1 != 0 ){
+            xor1 = xor1 & (xor1-1) ;
+
+            cnt ++ ;
+        }
+        return cnt ;
     }
 };
