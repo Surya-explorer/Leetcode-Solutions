b4b498252fb9ede1715016c02dadd5bc8028075f
Surya Prakash Gupta <113275807+Surya-explorer@users.noreply.github.com>
2024-05-30 23:26:52 +0530

    Time: 3 ms (83.37%) | Memory: 10.9 MB (78.16%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector <int> ans ;
+        
+        for (int i = 0 ; i<nums1.size() ; i++){
+            int j1 = 0   ;
+            auto it = std::find(nums2.begin(), nums2.end(), nums1[i]);
+            if (it != nums2.end()) {
+                j1 = std::distance(nums2.begin(), it);
+            }
+            for (int j = j1 ; j < nums2.size() ; j++ ){
+                if (nums1[i] < nums2[j]){
+                    ans.push_back(nums2[j]);
+                    break ;
+                }
+            }
+            if (ans.size() != i+1){
+                ans.push_back(-1) ;
+            }
+        }
+        return ans ;
+    }
+};
