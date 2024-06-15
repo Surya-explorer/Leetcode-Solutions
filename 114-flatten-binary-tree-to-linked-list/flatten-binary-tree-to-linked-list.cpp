26fd71422b00e81bacd2011fff1933c9f6cce7f2
Surya Prakash Gupta <113275807+Surya-explorer@users.noreply.github.com>
2024-06-15 19:46:20 +0530

    Time: 3 ms (77.28%) | Memory: 15.6 MB (86.67%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -12,70 +12,72 @@
 class Solution {
 public:
 
-    // void solve(TreeNode * root ) {
+    void solve(TreeNode * root ) {
         
-    //     if (root == NULL ) {
-    //         return   ;
-    //     }
+        if (root == NULL ) {
+            return   ;
+        }
 
-    //     if (root ->left) solve(root->left) ;
-    //     if (root ->right ) solve(root->right) ;
+        if (root ->left) solve(root->left) ;
+        if (root ->right ) solve(root->right) ;
         
-    //     if  (root ->left) {
-    //         TreeNode * rleft = root ->left ;
-    //         TreeNode * rright = root ->right ;
+        if  (root ->left) {
+            TreeNode * rleft = root ->left ;
+            TreeNode * rright = root ->right ;
 
-    //         root ->left = NULL ;
-    //         root ->right = rleft ;
+            root ->left = NULL ;
+            root ->right = rleft ;
             
-    //         TreeNode* current = root->right;
-    //         while (current->right) {
-    //             current = current->right;
-    //         }
-
-    //         Attach the saved right subtree
-    //         current->right = rright;
-    //     }
-         
-    // }
-
-    void flatten(TreeNode* root) {
-        // if (root == NULL) return ;
-        // solve (root)  ;
-        // return ;
-        // 'curr' to the root of the tree
-        TreeNode* curr = root;
-
-        // Iterate until 'curr'
-        // becomes NULL
-        while (curr) {
-            // Check if the current
-            // node has a left child
-            if (curr->left) {
-                // If yes, find the rightmost
-                // node in the left subtree
-                TreeNode* pre = curr->left;
-                while (pre->right) {
-                    pre = pre->right;
-                }
-
-                // Connect the rightmost node in
-                // the left subtree to the current
-               //  node's right child
-                pre->right = curr->right;
-
-                // Move the entire left subtree to the
-                // right child of the current node
-                curr->right = curr->left;
-
-                // Set the left child of
-                // the current node to NULL
-                curr->left = NULL;
+            TreeNode* current = root->right;
+            while (current->right) {
+                current = current->right;
             }
 
-            // Move to the next node
-            // on the right side
-            curr = curr->right;
+            // Attach the saved right subtree
+            current->right = rright;
         }
+         
+    }
+
+    void flatten(TreeNode* root) {
+        if (root == NULL) return ;
+        solve (root)  ;
+        return ;
+        
     }
 };
+        // // 'cur   // }
+        //  to the root of the tree
+        // TreeNode* curr = root;
+
+        // // Iterate until 'curr'
+        // // becomes NULL
+        // while (curr) {
+        //     // Check if the current
+        //     // node has a left child
+        //     if (curr->left) {
+        //         // If yes, find the rightmost
+        //         // node in the left subtree
+        //         TreeNode* pre = curr->left;
+        //         while (pre->right) {
+        //             pre = pre->right;
+        //         }
+
+        //         // Connect the rightmost node in
+        //         // the left subtree to the current
+        //        //  nodes right child
+        //         pre->right = curr->right;
+
+        //         // Move the entire left subtree to the
+        //         // right child of the current node
+        //         curr->right = curr->left;
+
+        //         // Set the left child of
+        //         // the current node to NULL
+        //         curr->left = NULL;
+        //     }
+
+        //     // Move to the next node
+        //     // on the right side
+        //     curr = curr->right;
+     
