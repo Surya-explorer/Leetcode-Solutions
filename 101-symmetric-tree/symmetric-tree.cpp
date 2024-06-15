98471f8aee5176eac7506a1ae9260d1cf5ec028e
Surya Prakash Gupta <113275807+Surya-explorer@users.noreply.github.com>
2024-06-15 15:44:06 +0530

    Time: 8 ms (12.54%) | Memory: 17.4 MB (38.00%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -12,7 +12,6 @@
 class Solution {
 public:
     void solve(TreeNode* node1 , TreeNode* node2 , bool &ans ){
-        
         if (!node1 && !node2) {
             return; // Both nodes are NULL, they are symmetric
         }
@@ -22,28 +21,20 @@ public:
         }
         solve(node1->left, node2->right, ans);
         solve(node1->right, node2->left, ans);
-        
-        // if ( node1->val != node2->val ) {
-        //     ans = false ;
-        //     return ;
-        // }
-        // if ( node1 && !node2 || !node1 && node2 ){
-        //     ans = false ;
-        //     return ;
-        // }
     }
     bool isSymmetric(TreeNode* root) {
         bool ans  = true ;
         if (root == NULL){
             return true ;
         }
-        if (root ->left == NULL && root ->right == NULL) return true ;
+       
         if (root ->left && root->right) {
             TreeNode* node1 = root ->left ;
             TreeNode* node2 = root ->right ;
             solve(node1 , node2 , ans ) ;
         }
         else {
+             if (root ->left == NULL && root ->right == NULL) return true ;
             return false ;
         }
         return ans ;
