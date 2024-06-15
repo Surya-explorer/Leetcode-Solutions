91ab79911c8c4273c220026f528ec59cba6300d4
Surya Prakash Gupta <113275807+Surya-explorer@users.noreply.github.com>
2024-06-15 14:25:13 +0530

    Time: 12 ms (60.25%) | Memory: 26 MB (43.03%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -21,7 +21,7 @@ public:
         TreeNode * root = inbuild (preorder, 0 , preorder.size() - 1 , inorder , 0 , inorder.size() -1 , inMap ) ;
         return root ;
     }
-    private : 
+    // private : 
     TreeNode * inbuild (vector<int>& preorder, int preStart ,int preEnd , vector<int>& inorder , int inStart ,int inEnd ,map <int , int >&inMap ) {
 
         if (preStart > preEnd ||  inStart > inEnd ){   //   
@@ -32,15 +32,10 @@ public:
         // It will return the index of the element in the inorder vector .
         // Before that element all the elements are for left subtree.
         // After that element all elements are for right subtree .
-
-
-        //////
         int gapleft = inroot - inStart ;
         root -> left = inbuild (preorder , preStart + 1 , preStart + gapleft , inorder , inStart  , inroot- 1 , inMap ) ;
         root -> right = inbuild (preorder ,  preStart + gapleft + 1 , preEnd  ,inorder , inroot + 1 , inEnd , inMap )  ;
 
-        // root -> left = inbuild (preorder , prestart + 1 , prestart + gap , inorder , instart  , inroot- 1 , inMap ) ;
-        // root -> right = inbuild (preorder , prestart + gap +  1 , preend , inorder ,inroot + 1 , inend , inMap )  ;
         return root ;
     }
 };
