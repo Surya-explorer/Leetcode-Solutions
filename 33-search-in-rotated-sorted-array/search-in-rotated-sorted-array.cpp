2e0376b0eb6fa10389403aef3854236441ec0261
Surya Prakash Gupta <113275807+Surya-explorer@users.noreply.github.com>
2024-06-20 14:20:26 +0530

    Time: 3 ms (59.57%) | Memory: 13.5 MB (37.17%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,17 +1,37 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n = nums.size() , check = 0 ;
-        int index = 0 ;
-        for ( int i = 0 ; i< n ; i++ ){
-            if (nums[i] == target ){
-                check = 1 ;
-                index = i ;
+        int n = nums.size();
+        int s = 0 ;
+        int e = n -1 ;
+        int mid = s + (e - s) / 2;
+        while (s <= e ){
+            mid = s + (e - s) / 2;
+            if (nums[mid] == target) return mid  ;
+            
+            // Check for left sorted array .
+            if (nums[s] <= nums[mid]) {
+                if ( nums[s] <= target && target < nums[mid]) e = mid -1 ;
+                else  s  = mid + 1 ;
             }
+            // Check for right sorted array .
+            else {
+                if (nums[mid] < target && target <= nums[e]) s = mid + 1 ;
+                else  e = mid - 1 ;
+            }
+            
         }
-        if (check == 0 ){
-            return -1 ;
-        }
-        return index ;
+        return -1 ;
     }
 };
+
+
+// for ( int i = 0 ; i< n ; i++ ){
+        //     if (nums[i] == target ){
+        //         check = 1 ;
+        //         index = i ;
+        //     }
+        // }
+        // if (check == 0 ){
+        //     return -1 ;
+        // }
