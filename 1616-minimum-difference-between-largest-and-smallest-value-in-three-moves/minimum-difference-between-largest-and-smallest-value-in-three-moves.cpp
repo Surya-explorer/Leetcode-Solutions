3382b7a10c2eabae9e196c77d6ab4bd690034aca
Surya Prakash Gupta <113275807+Surya-explorer@users.noreply.github.com>
2024-06-22 02:04:10 +0530

    Time: 67 ms (70.47%) | Memory: 38.9 MB (16.52%) - LeetSync

---
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minDifference(vector<int>& nums) {
+        int ans = 0 ;
+        int n = nums.size() ;
+        if (n <=4) return 0 ;
+
+        sort(nums.begin() ,nums.end()) ;
+        int result = INT_MAX;
+
+        // Compare the difference between the maximum and minimum
+        // after removing up to 3 smallest or largest elements
+        for (int i = 0; i <= 3; i++) {
+            result = std::min(result, nums[n - 4 + i] - nums[i]);
+        }
+        return result;
+    }
+};
