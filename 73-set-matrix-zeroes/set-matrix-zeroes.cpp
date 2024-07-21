b6d21c3b3f4b9c37a56c2bcba5bb151473dba0c2
Surya Prakash Gupta <113275807+Surya-explorer@users.noreply.github.com>
2024-07-21 17:53:06 +0530

    Time: 13 ms (21.50%) | Memory: 17 MB (5.02%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,42 +1,23 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m = matrix.size() ;  // Row 
-        int n = matrix[0].size() ; // Col
-        // Row --> matrix[0][]
-        // col --> matrix[][0]
-        int col1 = 1 ;
-        for (int i = 0 ; i< m ; i++ ){
-            for (int j = 0 ; j< n ; j++ ){
+        int n = matrix.size() ;
+        int m = matrix[0].size() ;
+        vector <int > row (n , -1 );
+        vector <int > col (m  ,-1) ;
+        for (int i = 0 ; i < matrix.size() ; i++){
+            for (int j = 0 ; j < matrix[0].size() ; j++){
                 if (matrix[i][j] == 0){
-                    matrix[i][0] = 0 ;
-                    if (j!= 0)
-                        matrix[0][j] = 0;
-                    else 
-                        col1 = 0 ;
+                   row[i] = 0 ;
+                   col[j] = 0 ;
                 }
             }
         }
-        for (int i = 1 ; i< m ; i++  ){
-            for (int j = 1 ; j< n ; j++){
-                if (matrix[i][j] != 0){
-                    // Check for the col and row 
-                    if (matrix[0][j] == 0 ||  matrix[i][0] == 0){
-                        matrix[i][j] = 0 ;
-                    }
-                }
+        for (int i = 0 ; i < matrix.size() ; i++){
+            for (int j = 0 ; j < matrix[0].size() ; j++){
+                if (row[i] == 0 || col[j] == 0) matrix[i][j] = 0 ;
             }
         }
-        if (matrix[0][0] == 0){
-            for (int j = 0 ; j < n ;j++ ){
-                matrix[0][j] = 0 ;
-            }
-        }
-        if (col1 == 0){
-            for (int i = 0 ; i < m ;i++ ){
-                matrix[i][0] = 0 ;
-            }
-        }
-        return ;
+
     }
 };
