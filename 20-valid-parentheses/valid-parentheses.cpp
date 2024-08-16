913a7c1d5cc8c30c666acb1f360846490c778844
Surya Prakash Gupta <113275807+Surya-explorer@users.noreply.github.com>
2024-08-17 00:39:37 +0530

    Time: 3 ms (40.84%) | Memory: 7.7 MB (70.95%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,27 +1,55 @@
 class Solution {
 public:
-    bool isValid(string s) {
-        int open = 0 ;
-        int close = 0 ;
 
-        stack<char> st ;
-        for (int i = 0 ; i< s.size() ; i++){
-            if (s[i] == '(' || s[i] == '{' || s[i] == '[' ) {
-                st.push(s[i]) ; 
-                open++ ;
+    void solve (string s , int opencnt , bool &ans , int index ){
+        
+        if (index == s.size()) return ;
+
+        for (int i =  index ; i < s.size() ; i++){
+            if (s[i] == '[' || s[i] == '(' || s[i] == '{' ) {
+                opencnt ++ ;
+                solve (s , opencnt , ans , index+1 );
+            }
+            if ( i > 0 ) {
+                if ( s[index] ==  '[' &&  s[i] == ']' || s[index] ==  '(' &&  s[i] == ')' || s[index] ==  '{' &&  s[i] == '}'){
+                    ans = true ;
+                    continue ;
+                }
+                else {
+                    ans = false  ;
+                }
+            }  
+        }
+    }
+    bool isValid(string s) {
+        int opencnt = 0 ;
+        // bool ans  = true ;
+        
+        stack <char> st ;
+
+        for (int i =  0 ; i < s.size() ; i++){
+            if (s[i] == '[' || s[i] == '(' || s[i] == '{' ) {
+                opencnt ++ ;
+                st.push(s[i]) ;
+                // solve (s , opencnt , ans , index+1 );
             }
             else {
-                if (st.empty()) return false  ; 
+                if (st.empty()) return false ;
                 char ch = st.top() ;
+                opencnt-- ;
+                
                 st.pop() ;
-                open-- ;
-
-                if (ch == '(' && s[i] == ')' || ch == '{' && s[i] == '}' || 
-                ch == '[' && s[i] == ']' )   continue ;
-                else return false ;
+                
+                if ( ch ==  '[' &&  s[i] == ']' || ch ==  '(' &&  s[i] == ')' 
+                   || ch ==  '{' &&  s[i] == '}') {
+                    continue ;
+                }
+                else {
+                    return false ;
+                }
             }
         }
-        if (open > 0 ) return false ;
+        if (opencnt > 0  ) return false ;
         return true ;
     }
 };
