f2f09326280f59a21cedc950c7ffe6944572cec8
Surya Prakash Gupta <113275807+Surya-explorer@users.noreply.github.com>
2024-08-17 22:33:53 +0530

    Time: 3 ms (89.28%) | Memory: 16.2 MB (60.38%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,47 +1,39 @@
 class Solution {
 public:
-
-    int firstoccurence (vector <int> nums , int target , int n ){
-        int s = 0, e = n - 1, mid;
-        int first = -1;
-        while (s <= e) {
-            mid = (s + e) / 2;
-            if (nums[mid] == target) {
-                first = mid;
-                e = mid - 1;
-            }
-            else if (nums[mid] > target) {
-                e = mid - 1;
-            }
-            else {
-                s = mid + 1;
-            }
-        }
-        return first ;
-    }
-    int lastoccurence (vector <int> nums , int target , int n ){
-        int s = 0, e = n - 1, mid;
-        int last = -1;
-        while (s <= e) {
-            mid = (s + e) / 2;
-            if (nums[mid] == target) {
-                last = mid;
-                s = mid + 1 ;
-            }
-            else if (nums[mid] > target) {
-                e = mid - 1;
-            }
-            else {
-                s = mid + 1;
-            }
-        }
-        return last ;
-    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        int n = nums.size() ;
-        int first = firstoccurence (nums , target , n) ;
-        if (first == -1 ) return {-1 , -1 } ;
-        int last = lastoccurence (nums , target , n) ;
-        return {first , last} ;
+        int s = 0;
+        int e = nums.size() - 1;
+
+        int m = 0;
+        vector<int> ans(2 , -1);
+        // if (nums.size() == 0 || nums.size() == 1) return {-1 , -1}  ;
+        while (s <= e) {
+            m = s + (e - s) / 2;
+            if (nums[m] == target) {
+                ans[0] = m;
+                e = m - 1;
+            } else if (nums[m] < target) {
+                s = m + 1;
+            } else {
+                e = m - 1;
+            }
+        }
+        s = 0;
+        e = nums.size() - 1;
+
+        while (s <= e) {
+            m = s + (e - s) / 2;
+            if (nums[m] == target) {
+                ans[1] = m;
+                s = m + 1;
+            } else if (nums[m] < target) {
+                s = m + 1;
+            } else {
+                e = m - 1;
+            }
+        }
+        if (ans.empty())
+            return {-1, -1};
+        return ans;
     }
 };
