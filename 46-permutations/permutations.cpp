e493368205f1c1da78e6ada2962e45a1e89554a2
Surya Prakash Gupta <113275807+Surya-explorer@users.noreply.github.com>
2024-08-18 19:32:42 +0530

    Time: 5 ms (31.62%) | Memory: 9.9 MB (40.96%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+    void solve( vector <vector <int> > &ans , vector<int>& nums ,  int index   , vector<int> v ){
+
+    if (index == nums.size()){
+        ans.push_back(nums) ;
+        return ;
+    }
+    for (int i = index ; i < nums.size() ; i++){
+
+        swap (nums[index] , nums[i]) ;
+        solve (ans , nums , index+ 1 , v) ;
+        swap (nums[index] , nums[i]) ;
+
+
+        // v.push_back(nums[i]) ;
+        // solve (ans , nums , index , v ) ;
+        // v.pop_back() ;
+        
+        // solve (ans , nums , index + 1 , v ) ;
+    }
+       
+}
+    vector<vector<int>> permute(vector<int>& nums) {
+        int index = 0 ;
+        vector <vector <int> > ans ;
+        vector <int> v = {} ;
+        solve (ans , nums , index , v ) ;
+
+        return ans ;
+    }
+};
