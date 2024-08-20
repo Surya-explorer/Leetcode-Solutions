81397c8843666f31f110ef94e0d592a0b81feb16
Surya Prakash Gupta <113275807+Surya-explorer@users.noreply.github.com>
2024-08-20 23:46:14 +0530

    Time: 103 ms (80.15%) | Memory: 80 MB (67.91%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,40 +1,73 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
+        int maxarea = 0 ;
+        stack <int > s ;
+        for (int i = 0 ; i < heights.size() ; i++){
+            while (!s.empty() && heights[s.top()] > heights[i]){
+                int h = heights[s.top()] ;
+                s.pop() ;
+                int  w = s.empty() ? i : i- s.top() - 1 ;
+                int area = h* w ;
+                maxarea = max(maxarea ,  area );
+            }
+            s.push(i) ;
+        }
         int n = heights.size() ;
-        vector <int > left(n) ;     
-        vector <int > right(n)  ;
-        stack <int> st ;
-
-        // finding left smaller elements
-        for (int i = 0 ; i < heights.size() ; i++){ 
-            while(!st.empty() && heights[st.top()] >= heights[i]){
-                st.pop() ;
-            } 
-            if (st.empty()) left[i] = 0 ;
-            else left[i] = st.top() + 1 ;
-            st.push(i) ;
-        }  
-        
-        // Removing Left numbers in stack .
-        while (!st.empty() ) st.pop() ;
-        
-        // finding right smaller elements
-        for (int i = heights.size()-1 ; i >= 0 ; i--){     
-            while  (!st.empty() && heights[st.top()] >= heights[i]){
-                st.pop() ;
-            } 
-            if (st.empty()) right[i] = n-1 ;
-            else right[i] = st.top() - 1 ;
-            // right[i] = (st.empty()) ? n - 1 : st.top() - 1;
-            st.push(i) ;
-        }     
-        int Max = 0 ;
-        for (int i =  0 ; i < n ; i++){
-            int ele = (right[i] - left[i] + 1) ;
-
-            Max = max ( Max , ele * heights[i])  ;
-        } 
-        return Max ;
+        while (!s.empty()){
+            int h = heights[s.top()] ;
+            
+            s.pop() ;
+            int  w = s.empty() ? n :  n - s.top() - 1 ;
+            maxarea = max(maxarea , h * w  ) ;            
+        }
+        return maxarea ;
     }
 };
+
+
+
+// if (heights.size() == 1 ) return heights[0] ;
+//         if (heights.size() == 0 ) return 0  ;
+//         for (int i = 0 ; i+1 < heights.size() ; i++){
+//             if ( heights[i] <= heights[i+1]){
+//                 int index = i+1 ;
+//                 while (index < heights.size()  && heights[i] <= heights[index] ){
+//                     index++ ;
+//                 }
+//                 int area = heights[i] *(index-i) ;
+//                 maxarea = max(maxarea , area   )  ;
+//             }
+//             else {
+//                 int area1 = min(heights[i] , heights[i+1]) * 2 ;
+//                 maxarea = max(maxarea , area1 )  ;
+                
+//             }
+//             minheight = min (minheight , heights[i]) ;
+//             minheight = min (minheight , heights[i+1]) ;
+            
+//             int area2 = min(heights[i] , heights[i+1]) * 2 ;
+//             maxarea = max(maxarea  , area2 ) ;
+            
+//             maxarea = max(maxarea  , heights[i]) ;
+//             maxarea = max(maxarea  , heights[i+1]) ;
+            
+//         }
+//         int allarea = minheight*(heights.size()) ;
+//         maxarea = max(maxarea  , allarea ) ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
