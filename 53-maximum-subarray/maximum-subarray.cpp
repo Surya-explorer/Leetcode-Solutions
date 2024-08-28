c66d8a6114701a07a2942aa5860763202407a84e
Surya Prakash Gupta <113275807+Surya-explorer@users.noreply.github.com>
2024-08-28 22:03:43 +0530

    Time: 79 ms (45.61%) | Memory: 70.6 MB (60.22%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -3,22 +3,24 @@ public:
     int maxSubArray(vector<int>& nums) {
         
         int sum =0 ;
-        int maxsum = 0 ;
+        int maxsum = INT_MIN ;
         int cnt = 0 ;
-        int maxi = INT_MIN ;
+        // int maxi =  ;
+        // if nums contains only negative numbers .
+        // for (int i = 0 ; i < nums.size()  ; i++){
+        //     if (nums[i] < 0 ) cnt++ ;
+        //     maxi = max (maxi , nums[i]) ;
+        // }
 
-        for (int i = 0 ; i < nums.size()  ; i++){
-            if (nums[i] < 0 ) cnt++ ;
-            maxi = max (maxi , nums[i]) ;
-        }
+        // if (cnt == nums.size()) return maxi ;
 
-        if (cnt == nums.size()) return maxi ;
+        // if nums contains mix of negative and postive numbers .
         for (int i = 0 ; i < nums.size() ; i++){
+            
             sum+= nums[i] ;
-
+            maxsum = max (maxsum , sum ) ;
             if (sum < 0 ) sum= 0 ;
 
-            maxsum = max (maxsum , sum ) ;
         }
         return maxsum ;
 
